e4da2ed4bbec97fb5f4dc0194abb49c908443b70
hamyngn <hamyngn@gmail.com>
2023-09-07 17:12:03 +0200

    add: dist folder

---
+++ dist/hooks/useFocus.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.useFocus = useFocus;
+var _react = require("react");
+/**
+ * set focus to first list item or selected item
+ * @param {Array} list 
+ * @param {boolean} showList 
+ * @param {number} selectedIndex 
+ * @param {number} focusedItemIndex 
+ * @param {*} refButton 
+ * @param {*} listRef 
+ */
+function useFocus(list, showList, selectedIndex, focusedItemIndex, refButton, listRef) {
+  (0, _react.useEffect)(() => {
+    // set focus to first list item if there is no selected item
+    if (list && showList && selectedIndex === null && (focusedItemIndex || focusedItemIndex === 0)) {
+      refButton.current.blur();
+      listRef[focusedItemIndex].current.focus();
+    }
+
+    // if there is selected item and menu is opened, set focus on selected list item
+    if (showList && selectedIndex) {
+      refButton.current.blur();
+      listRef[selectedIndex].current.focus();
+    }
+  }, [list, showList, selectedIndex, listRef, focusedItemIndex, refButton]);
+}
